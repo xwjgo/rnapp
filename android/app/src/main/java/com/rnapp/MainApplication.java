7cb749f0808da17e8016efe4c2fc8808d9193d5d
xwjgo <wenjiangxu@126.com>
2017-05-03 18:40:48 +0800

    dev

--- android/app/src/main/java/com/rnapp/MainApplication.java
+++ android/app/src/main/java/com/rnapp/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.github.yamill.orientation.OrientationPackage;
 
 import java.util.Arrays;
 import java.util.List;
